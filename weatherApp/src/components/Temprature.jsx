f83a796712ae2882c917c3e7d43f2cb27846ed0e
chandrashekarR <158203171+chandrashekarRravi@users.noreply.github.com>
2025-03-24 20:49:36 +0530

    Add files via upload

---
+++ weatherApp/src/components/Temprature.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Temprature = ({ setCity, stats }) => {
+    const handlecitychange = (e) => {
+        setCity(e.target.value);
+    }
+    return (
+        <>
+            <div className='flex justify-between'>
+                <input type="search" className='bg-[#feeb80]  text-[#191300]
+                placeholder-current-slate-500s placeholder-salte-900 text-md w-60 p-2 focus:outline-none focus:border-gray-300   rounded-full text-tranfrom scale-100 hover:scale-110 transition-transform duration-300  cursor-pointer' placeholder='Enter Your City Name  ' onChange={handlecitychange}
+                    defaultValue="Shimoga " />
+                <div className='m-4'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" className="w-6 h-6  text-tranfrom scale-100 hover:scale-110 transition-transform duration-300  cursor-pointer">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                    </svg>
+
+                </div>
+            </div>
+            <div className='flex justify-center mt-8'>
+                {stats.isDay !== 0 ? (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-14 h-14 m-4 text-yellow-500 text-tranfrom scale-100 hover:scale-110 transition-transform duration-300  cursor-pointer">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+                </svg>
+                ) : (
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-12 w-12 m-4 text-[#a3b4c4] text-tranfrom scale-100 hover:scale-110 transition-transform duration-300 ease-in-out">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+                    </svg>
+                )}
+
+
+            </div>
+            <div className='flex justify-center mt-8 items-center  text-[#191300]'>
+                <p className='font-bold text-[55px] text-tranfrom scale-100 hover:scale-110 transition-transform duration-300 ease-in-out   '>
+                    {stats.temp} </p> <span className='text-[33px] '>ºC</span>
+            </div>
+            <div className='flex justify-center mt-8 text-[25px] items-ceneter text-slate-500text-tranfrom scale-100 hover:scale-110 transition-transform duration-300 ease-in-out'>
+                {stats.condition}
+            </div>
+            <div className='flex justify-center mt-5 text-[20px] text-[#191300] items-ceneter  '>
+                Today {stats.time} | {stats.location}
+            </div>
+        </>
+    )
+}
+
+export default Temprature
